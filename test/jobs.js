8298f1c5b59a24afcd2b7428b6495edb1729d0bb
rajkharvar <rajkharvar1808@gmail.com>
2022-07-28 18:29:35 +0530

    Done with JSON selector, need to fix XPATH selector tests

--- test/jobs.js
+++ test/jobs.js
@@ -2,9 +2,14 @@
 const jobsData = require("../jobsData.json");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const _ = require("lodash");
+const axios = require("axios").default;
+const DOMParser = require("dom-parser");
+const jsdom = require("jsdom");
 
 chai.use(chaiHttp);
 const { expect } = chai;
+const { JSDOM } = jsdom;
 
 const keysWithType = {
   weight: "number",
@@ -38,7 +43,7 @@ describe("Jobs test", () => {
     });
   });
 
-  it("Job url should send OK response", async () => {
+  it("Job URL should send OK response", async () => {
     await Promise.all(
       jobsData.map(async (job, index) => {
         const { url, name } = job;
@@ -48,6 +53,42 @@ describe("Jobs test", () => {
           200,
           `Job(${name}) url should send OK response`
         );
+      })
+    );
+  });
+
+  it("Job selector should be present in URL response", async () => {
+    await Promise.all(
+      jobsData.map(async (job, index) => {
+        const { selectorType, url, name, selector } = job;
+
+        // JSON
+        if (selectorType === 0) {
+          const res = await chai.request(url).get("");
+          const result = _.get(res.body, selector);
+          expect(isNaN(result)).to.be.eq(
+            false,
+            `Job[${name}] does not have required selector`
+          );
+        } else if (selectorType === 1) {
+          const res = await axios.get(url, {
+            headers: {
+              "Content-Type": "application/xml; charset=utf-8",
+            },
+          });
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(res.data, "text/html");
+          const dom = new JSDOM(res.data);
+          const result = dom.window.document.evaluate(
+            selector,
+            doc,
+            null,
+            dom.window.XPathResult.FIRST_ORDERED_NODE_TYPE,
+            null
+          ).singleNodeValue;
+
+          console.log(result);
+        }
       })
     );
   });
