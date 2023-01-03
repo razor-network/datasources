4fc5e100549b0df99f1e23a68db78f571bb7a578
rajkharvar <rajkharvar1808@gmail.com>
2023-01-03 14:43:01 +0530

    Added tests for datasources result deviation

--- test/jobs.js
+++ test/jobs.js
@@ -1,5 +1,5 @@
-//! Replace jobsData.json -> jobs.json before merging to master
 const jobsData = require("../jobs.json");
+const collectionData = require("../collections.json");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const _ = require("lodash");
@@ -18,6 +18,25 @@ const keysWithType = {
   name: "string",
   selector: "string",
   url: "string",
+};
+
+// percentage
+const THRESHOLD = 20;
+
+const getMedian = (values) => {
+  if (values.length === 0) throw new Error("No inputs");
+  values.sort(function (a, b) {
+    return a - b;
+  });
+  let half = Math.floor(values.length / 2);
+  if (values.length % 2) return values[half];
+
+  return (values[half - 1] + values[half]) / 2.0;
+};
+
+const getDeviation = (value, median) => {
+  let change = value / median - 1;
+  return Math.abs(change.toFixed(2)) * 100;
 };
 
 const requiredKeys = Object.keys(keysWithType).sort();
@@ -113,7 +132,7 @@ describe("Jobs test", () => {
             `Job[${name}] does not have required selector`
           );
 
-          tableData.push({ job: name, result });
+          tableData.push({ id: index + 1, job: name, result });
         }
       })
     );
@@ -156,10 +175,42 @@ describe("Jobs test", () => {
             `Job[${name}] does not have numeric result`
           );
 
-          tableData.push({ job: name, result: trimResult });
+          tableData.push({ id: index + 1, job: name, result: trimResult });
         }
       })
     );
+  });
+
+  it("Job value should not deviate more than threshold percentage", () => {
+    collectionData.map((collection, index) => {
+      let jobValues = [];
+      const { jobIDs } = collection;
+
+      // calculate median for each collection
+      jobIDs.map((id, i) => {
+        const job = tableData.find((o) => o.id === id);
+        expect(job).to.be.an(
+          "object",
+          `In collection "${collection.name}", JobId "${id}" was not found`
+        );
+        jobValues.push(parseFloat(job.result));
+      });
+      let median = getMedian(jobValues);
+
+      // compare each job value with median
+      jobIDs.map((id, i) => {
+        const job = tableData.find((o) => o.id === id);
+        let { result } = job;
+        result = parseFloat(result);
+        const deviation = getDeviation(result, median);
+        expect(deviation).to.be.lt(
+          THRESHOLD,
+          `In collection "${collection.name}", JobId "${id}" result is deviating by ${deviation} %`
+        );
+      });
+    });
+
+    tableData.sort((a, b) => (a.id > b.id ? 1 : -1));
   });
 
   after(() => console.table(tableData));
